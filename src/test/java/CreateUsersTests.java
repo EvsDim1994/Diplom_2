0cf87ca7c9a2dc6e6dfab3932adc1e411306e320
evsyukov.dv <dmitriy.evsyukov@sbermarket.ru>
2025-04-04 16:08:09 +0300

    add API test + allure report

---
+++ src/test/java/CreateUsersTests.java
@@ -0,0 +1,76 @@
+import io.qameta.allure.junit4.DisplayName;
+import model.User;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+public class CreateOrderTests extends BaseTest {
+
+    @Test
+    @DisplayName("Успешное создание пользователя")
+    public void createUserTest() {
+        var user = User.random();
+        userToken = requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("accessToken");
+        assertNotNull(userToken);
+    }
+
+    @Test
+    @DisplayName("Ошибка при создание существующего пользователя")
+    public void createUserWithTheSameDateTest() {
+        var user = User.random();
+        userToken = requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("accessToken");
+        requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(403)
+                .body("message", equalTo("User already exists"));
+    }
+
+    @Test
+    @DisplayName("Ошибка при создание пользователя без email")
+    public void createUserWithOutEmail() {
+        var user = User.random();
+        user.setEmail(null);
+        requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(403)
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @Test
+    @DisplayName("Ошибка при создание пользователя без password")
+    public void createUserWithOutPassword() {
+        var user = User.random();
+        user.setPassword(null);
+        requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(403)
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @Test
+    @DisplayName("Ошибка при создание пользователя без name")
+    public void createUserWithOutName() {
+        var user = User.random();
+        user.setName(null);
+        requests.createUser(user)
+                .then()
+                .assertThat()
+                .statusCode(403)
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+}
